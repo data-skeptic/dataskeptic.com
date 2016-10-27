ae8f3a07b47b2d6424283166660b376a68ad6a63
Billy Everyteen <kyle@datascience.com>
2016-10-26 22:55:04 -0700

    stub out social media stuff

---
+++ js/components/SocialMediaCard.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default class SocialMediaCard extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	
+	render() {
+		return (
+			<div>
+				<p>Follow Data Skeptic on <a href="https://twitter.com/dataskeptic">Twitter</a></p>
+				<p>Like us on <a href="https://www.facebook.com/dataskeptic">Facebook</a></p>
+			</div>
+		)
+	}
+}
