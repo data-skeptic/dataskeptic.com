ef48d9c1124baf020b773780ac498c3a7b1a3326
Billy Everyteen <kyle@datascience.com>
2016-11-28 19:05:41 -0800

    server

---
+++ js/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var React = require('react')
+var ReactDOMServer = require('react-dom/server')
+var Component = require('./Component.jsx')
+
+app.get('/', function(request, response) {
+  var html = ReactDOMServer.renderToString(
+    React.createElement(Component)
+  );
+  response.send(html)
+})
+
+var PORT = 3000
+app.listen(PORT, function() {
+  console.log('http://localhost:' + PORT)
+})
