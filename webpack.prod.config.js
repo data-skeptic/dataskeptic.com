3192cd0fbfd6d627bd702ba72f05e54206aabacb
Billy Everyteen <kyle@datascience.com>
2016-12-13 14:25:41 -0800

    changing build location

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extensions:         ['', '.js', '.jsx']
   },
   output: {
-    path:       path.join(__dirname, 'dist'),
+    path:       path.join(__dirname, 'public'),
     filename:   'bundle.js',
     publicPath: '/'
   },
