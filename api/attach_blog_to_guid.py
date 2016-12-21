cf343f9a7f34a931bc2b54961649a2779058661e
Billy Everyteen <kyle@datascience.com>
2016-12-21 11:40:14 -0800

    syncing

--- api/attach_blog_to_guid.py
+++ api/attach_blog_to_guid.py
@@ -1,19 +1,52 @@
+import requests
+import xmltodict
+import os
 import boto3
+
+fname = 'feed.rss'
+url = 'http://dataskeptic.com/feed.rss'
+
+r = requests.get(url)
+
+xml = xmltodict.parse(r.content)
+episodes = xml['rss']['channel']['item']
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('blog')
+blogs = table.scan()
+blogs = blogs['Items']
+
+bmap = {}
+
+for blog in blogs:
+	pn = blog['prettyname']
+	if pn.find('/episodes/') == 0:
+		bmap[pn] = blog
 
 envs = ["", "dev."]
-uri = "dataskeptic.com/episodes/2016/ms-connect-conference.md"
-guid = "fd9c484eff435ece4da87c3fc3acebe1"
 
 for env in envs:
-	u = env + uri
-	print(u)
-	response = table.update_item(
-		Key = {'uri': u},
-		UpdateExpression='set guid = :g',
-		ExpressionAttributeValues={ ':g': guid},
-		ReturnValues="UPDATED_NEW"
-	)
-	print(response)
+	for episode in episodes:
+		link = episode['link']
+		if link.find('http://dataskeptic.com/epnotes') == -1:
+			x = len('http://dataskeptic.com/blog')
+			link = link[x:]
+			try:
+				blog = bmap[link]
+				if not(blog.has_key('guid')):
+					guid = episode['guid']['#text']
+					uri = blog['uri']
+					print("-------------------")
+					print([guid, uri])
+					response = table.update_item(
+						Key = {'uri': uri},
+						UpdateExpression='set guid = :g',
+						ExpressionAttributeValues={ ':g': guid},
+						ReturnValues="UPDATED_NEW"
+					)
+					print(response)
+			except ValueError:
+				print('Cannot find blog post for ' + link)
+
+
+
--- shared/components/BlogListItem.js
+++ shared/components/BlogListItem.js
@@ -19,24 +19,33 @@ class BlogListItem extends Component {
         }
     }
     render() {
+        var monthNames = [
+          "January", "February", "March",
+          "April", "May", "June", "July",
+          "August", "September", "October",
+          "November", "December"
+        ];
         var onClick = this.onClick
     	var blog = this.props.blog
         var pn = blog.prettyname
+        var datestr = ""
         if (pn != undefined) {
             if (pn[0] == "/") {
                 pn = pn.substring(1, pn.length)
             }
             pn = "/blog/" + pn
             var date = blog["publish_date"]
+            date = new Date(date)
+            datestr = monthNames[date.getMonth()].toUpperCase() + " " + date.getDate() + ", " + (date.getYear()+1900)
         }
         return (
             <div className="col-xs-12">
 	            <div className="blog-summary" key={blog.uri}>
+                    <div className="blog-date">{datestr}</div>
 	                <Link onClick={this.onClick.bind(this)} className="blog-title" to={pn}>{blog.title}</Link>
-	                <span className="blog-date">{date}</span>
-	                <p>
+	                <p className="blog-desc">
                         {blog.desc}
-                        ... <Link className="blog-title" to={pn}>[more]</Link>
+                        ... <Link className="blog-view-more" to={pn}>View More &gt;</Link>
                     </p>
 	            </div>
             </div>
