7b2fc1e68ff00ecf4ece04206e7ea4eea09848ff
Billy Everyteen <kyle@datascience.com>
2017-01-03 22:02:57 -0800

    adding paypal option

--- shared/components/Membership.jsx
+++ shared/components/Membership.jsx
@@ -38,7 +38,7 @@ class Membership extends Component {
 			return (
 				<div className="center">
 					<h2>Data Skeptic Membership</h2>
-					<p>Are you a corporation or group?  <a href="#corporate">Check here</a>.</p>
+					<p>Are you a corporation or group?  Contact <a href="mailto:kyle@dataskeptic.com">kyle@dataskeptic.com</a> regarding opportunties to partner with Data Skeptic.</p>
 					<p>Your membership supports Data Skeptic's ability to continue delivering quality content on a weekly basis and expand into new mediums.  For $1 per episode, your contributions can help us launch more projects and continuously improve the content of the podcast.</p>
 					<p>We have some surprise members only perks planned for 2017.  Sign up now so you don't miss out.  If you'd like, you can play <Link to="/lightsout">lights out</Link> on this site.</p>
 					<h3>Monthly Memberships</h3>
@@ -56,32 +56,13 @@ class Membership extends Component {
 												<div className="membership-price">${product.price} <span className="per_month">/ month</span></div>
 												<div className="membership-btn">
 													<button className="membership-add" onClick={me.addToCart.bind(me, product)}>Add to Cart</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								)
-							}
-						})}
-					</div>
-					<div className="clear"></div>
-					<a name="corporate" />
-					<h3>Corporate Memberships</h3>
-					<p>We reserve the right to gratiously decline sponsors for any reason including but not limited to companies whose products are pornographic, offensive, etc.  More realistically, we may regret to decline sponsors due to existing exclusivity agreements.</p>
-					<div className="row">
-						{products.map(function(product) {
-							if (product.active == 1 && product.type=="membership" && product.price >= 128) {
-								var pid = product.id
-								return (
-									<div key={pid} className="col-xs-12 col-sm-4 membership-container">
-										<div className="membership-inner">
-											<img className="membership-img" src={product.img} />
-											<div className="membership-title">{product.title}</div>
-											<div className="membership-desc">{product.desc}</div>
-											<div className="membership-bottom-container">
-												<div className="membership-price">${product.price} <span className="per_month">/ month</span></div>
-												<div className="membership-btn">
-													<button className="membership-add" onClick={me.addToCart.bind(me, product)}>Add to Cart</button>
+													<p>or</p>
+													<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+														<input type="hidden" name="cmd" value="_s-xclick" />
+														<input type="hidden" name="hosted_button_id" value={product.paypal} />
+														<input type="submit" className="membership-add-paypal" value="Subscribe with Paypal" />
+														<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+													</form>
 												</div>
 											</div>
 										</div>
