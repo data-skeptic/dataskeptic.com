0ac4db4b29dc11956634f2a601cff87aeb74d90f
Billy Everyteen <kyle@datascience.com>
2017-01-05 22:47:31 -0800

    blog reducer updates

--- shared/daos/getBlog.js
+++ shared/daos/getBlog.js
@@ -6,8 +6,9 @@ export default function getBlog(dispatch, env, prettyname) {
 	var db_env = env
 	if (db_env == "prod") {
 		db_env = "master"
+	} else {
+		console.log("Network: retrieving single blog metadata")		
 	}
-	console.log("Network: retrieving single blog metadata")
 	var uri = "https://obbec1jy5l.execute-api.us-east-1.amazonaws.com/" + env + "/blog?env=" + db_env + "&pn=" + prettyname
 	// TODO: check cache
 	axios
@@ -18,7 +19,13 @@ export default function getBlog(dispatch, env, prettyname) {
 				var uri = blog["uri"]
 				if (uri != undefined) {
 					dispatch({type: "ADD_BLOG", payload: {blog, dispatch} })					
+				} else {
+					console.log("Did not get blog back as expected")
+					console.log(result)
 				}
+			} else {
+				console.log("Didn't get blog back as expected")
+				console.log(result)
 			}
 		})
 		.catch((err) => {
