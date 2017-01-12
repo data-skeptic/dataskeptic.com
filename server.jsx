6ccc672a131ab04c5196ea6763aea3bf13f29659
Billy Everyteen <kyle@datascience.com>
2017-01-12 10:01:43 -0800

    modularizing the code

--- server.jsx
+++ server.jsx
@@ -1,5 +1,6 @@
 import aws                       from 'aws-sdk'
 import axios                     from 'axios';
+import {join_slack}              from 'backend/join_slack'
 import bodyParser                from 'body-parser'
 import compression               from 'compression';
 import getBlogs                  from 'daos/blogs';
@@ -94,46 +95,10 @@ fs.open("config.json", "r", function(error, fd) {
   })
 })
 
-function serialize(obj) {
-  var str = [];
-  for(var p in obj)
-     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-  return str.join("&");
-}
-
 function api_router(req, res) {
   if (req.url.indexOf('/api/slack/join') == 0) {
     var req = req.body
-    var config = {}
-    req['token'] = slack_key
-    var sreq = serialize(req)
-    axios
-      .post("https://dataskeptic.slack.com/api/users.admin.invite?" + sreq, req, config)
-      .then(function(resp) {
-        var data = resp['data']
-        var msg = ""
-        if (data.ok) {
-          msg = "Welcome to our Slack channel.  You should receive a confirmation email shortly!"
-          var resp = {msg}
-          return res.status(200).end(JSON.stringify(resp))
-        } else {
-          var error = data.error
-          if (error == "already_invited") {
-            msg = "You have already been invited to our Slack channel.  Please check your spam folder or search your email to find the invite."
-          } else if (error == "already_in_team") {
-            msg = "You are already a member of the team.  Visit https://dataskeptic.slack.com/ to log in."
-          } else {
-            msg = "An error has occured.  Please contact kyle@dataskeptic.com for assistance."
-          }
-          var resp = {msg}
-          return res.status(400).end(JSON.stringify(resp))
-        }
-      })
-      .catch(function(err) {
-        console.log(err)
-        var resp = {err, "msg": "We could not process your request.  Please contact kyle@dataskeptic.com for assistance"}
-        return res.status(400).end(JSON.stringify(resp))
-      })
+    join_slack(req, res, slack_key)
   }
 }
 
