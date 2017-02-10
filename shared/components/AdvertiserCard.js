4746a53ca712af3f7e91de9b7559787cc99d7c25
Billy Everyteen <kyle@datascience.com>
2017-02-09 23:54:27 -0800

    update advertiser card'

--- shared/components/AdvertiserCard.js
+++ shared/components/AdvertiserCard.js
@@ -9,12 +9,15 @@ export default class AdvertiserCard extends React.Component {
 	render() {
 		return (
 			<div className="advertiser-card">
-				<br/>
-				<center><a href="https://www.periscopedata.com/skeptics"><img src="/blog/episodes/2017/src-data-provenance-and-reproducibility-with-pachyderm/periscope-data.jpg" /></a></center>
-				<br/><br/>
-				<p>Thanks to Periscope Data for sponsoring this week's episode of Data Skeptic.</p>
-				<p>Please visit <a href="https://www.periscopedata.com/skeptics">https://www.periscopedata.com/skeptics</a><br/>
-				to learn more about what you can do with their tools.</p>
+				<div className="row">
+					<div className="col-xs-12">
+					<br/>
+						<center><img src="https://s3.amazonaws.com/dataskeptic.com/img-static/data-sci-assn.png" width="100" /></center>
+						<br/>
+						<p>Thanks to the Data Science Association for sponsoring this week's episode of Data Skeptic.</p>
+						<p>Please visit <a href="http://dallasdatascience.eventbrite.com">dallasdatascience.eventbrite.com</a> to learn about their upcoming conference on Saturday February 18th.</p>
+					</div>
+				</div>
 			</div>
 		)
 	}
