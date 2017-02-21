0973e70d4571605bd9176c47f0765acd8d5a3ef5
Gleb L <lozhkovoygleb@icloud.com>
2017-02-22 00:02:31 +0200

    New checkout page (#53)
    * Application launch fixes
    
    * Initial checkout page structure changes
    
    * Reset checkout forms styles
    
    * Complete styles for shipping form
    
    * Done with checkout page styling
    
    * Mobile / Tablet view checkout page improvements

--- shared/components/Checkout.js
+++ shared/components/Checkout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
 
@@ -10,7 +10,7 @@ import Loading from './Loading'
 import SizeSelector from './SizeSelector'
 import ThankYouPage from './ThankYouPage'
 
-class Checkout extends React.Component {
+class Checkout extends Component {
 	constructor(props) {
 		super(props)
 	}
@@ -24,13 +24,14 @@ class Checkout extends React.Component {
 	}
 
 	render() {
-		var ocart = this.props.cart.toJS()
-		var cart_items = ocart.cart_items
-		var country_long = ocart.country_long
-		var address = ocart.address
-		var stripeLoading = ocart.stripeLoading
-		var stripeLoadingError = ocart.stripeLoadError
-		var paymentComplete = ocart.paymentComplete
+		const ocart = this.props.cart.toJS();
+		const cart_items = ocart.cart_items;
+		const country_long = ocart.country_long;
+		const address = ocart.address;
+		const stripeLoading = ocart.stripeLoading;
+		const stripeLoadingError = ocart.stripeLoadError;
+		const paymentComplete = ocart.paymentComplete;
+
 		if (stripeLoading) {
 			return <div><Loading /></div>
 		}
@@ -44,24 +45,20 @@ class Checkout extends React.Component {
 			return <CartContainer updateable={true} />
 		}
 		else {
-			var me = this
-			var checkout = <div></div>
 			return (
-				<div className="center">
-					<CartContainer updateable={false} />
-					<div>
-						<h2>Checkout</h2>
-					    <div className="col-xs-12">
-					        <AddressForm title="Shipping Address" />
-					    </div>
-					    <div>&nbsp;</div>
-					    <div className="col-xs-12">
-					        <CreditCardForm />
-					    </div>
-					    <div className="clear"></div>
+				<div className="checkout-page">
+					<div className="checkout-cart">
+						<CartContainer updateable={true} />
 					</div>
-				    <div>&nbsp;</div>
-				    <div>&nbsp;</div>
+					<div className="checkout-form">
+						<div className="inner">
+							<h2>Checkout</h2>
+
+							<AddressForm title="Shipping Information" />
+							<CreditCardForm title="Billing Information"/>
+							<div className="clear"></div>
+						</div>
+					</div>
 				</div>
 			)
 		}
