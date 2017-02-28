8ec47e507c884fcd89de0e3a47048afe28abeaab
glozhkovoy <lozhkovoygleb@icloud.com>
2017-02-28 08:43:56 +0200

    Main navigation active link improvements

--- shared/components/Menu.js
+++ shared/components/Menu.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
 
 import NavLink from './NavLink'
 
-class Menu extends React.Component {
+class Menu extends Component {
+
     constructor(props) {
         super(props)
     }
 
+    isLinkActive(link) {
+        const {pathname} = this.props;
+
+        return pathname.indexOf(link) === 0;
+    }
+
     render() {
-        const {pathname, itemClick, children} = this.props
+        const {itemClick, children} = this.props;
+
+        const links = [
+            { to: '/podcast', name: 'Podcasts' },
+            { to: '/blog', name: 'Blog' },
+            { to: '/projects', name: 'Projects' },
+            { to: '/services', name: 'Services' }
+        ];
 
         return (
             <div className="nav">
                 {children}
-                <NavLink active={pathname} to="/podcast" onClick={itemClick}>Podcasts</NavLink>
-                <NavLink active={pathname} to="/blog" onClick={itemClick}>Blog</NavLink>
-                <NavLink active={pathname} to="/projects" onClick={itemClick}>Projects</NavLink>
-                <NavLink active={pathname} to="/services" onClick={itemClick}>Services</NavLink>
+
+                {links.map(({to, name}, ind) => <NavLink keu={ind} isActive={this.isLinkActive(to)} to={to} onClick={itemClick}>{name}</NavLink>)}
             </div>
         )
     }
+
 }
 
 export default Menu;
