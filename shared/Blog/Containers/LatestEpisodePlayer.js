4c10a2896270d753971e97362c42e130013cfaed
Gleb L <lozhkovoygleb@icloud.com>
2017-03-01 23:27:49 +0200

    Site styles updates (#73)

--- shared/Blog/Containers/LatestEpisodePlayer.js
+++ shared/Blog/Containers/LatestEpisodePlayer.js
@@ -1,65 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import moment from 'moment';
+
+import { Link } from 'react-router';
 
 import Loading from "../../Common/Components/Loading"
 import Error from "../../Common/Components/Error"
 import {loadEpisode} from "../../utils/redux_loader"
 
-class LatestEpisodePlayer extends React.Component {
+class LatestEpisodePlayer extends Component {
 	constructor(props) {
 		super(props)
 	}
 
 	componentWillMount() {
-		var dispatch = this.props.dispatch
-		var episodes = this.props.episodes.toJS()
-		var focus_episode = episodes.focus_episode
-		var guid = this.props.guid
-		if (guid != undefined) {
+		var dispatch = this.props.dispatch;
+		var episodes = this.props.episodes.toJS();
+		var focus_episode = episodes.focus_episode;
+		var guid = this.props.guid;
+		if (!guid) {
 			loadEpisode(guid, dispatch)
 		}
 	}
 
 	onClick(episode) {
-		this.props.dispatch({type: "PLAY_EPISODE", payload: episode })		
+		this.props.dispatch({type: "PLAY_EPISODE", payload: episode });
 	}
 
 	render() {
-		var guid = this.props.guid
-		if (guid == undefined) {
-			return <div></div>
+		const guid = this.props.guid;
+		if (!guid) {
+			return null;
 		}
-		var oplayer = this.props.player.toJS()
-		var playback_loaded = oplayer.playback_loaded
-		var episodes = this.props.episodes.toJS()
-		var focus_episode = episodes.focus_episode
-		var loaded = focus_episode.loaded
-		var episode = focus_episode.episode
-		if (loaded == -1) {
+		var oplayer = this.props.player.toJS();
+		var playback_loaded = oplayer.playback_loaded;
+		var episodes = this.props.episodes.toJS();
+		var focus_episode = episodes.focus_episode;
+		var loaded = focus_episode.loaded;
+		var episode = focus_episode.episode;
+		if (loaded === -1) {
 			return <Error />
 		}
-		else if (loaded == 0) {
+		else if (loaded === 0) {
 			return <Loading />
 		}
-		else if (episode == undefined) {
+		else if (episode === undefined) {
 			return <Error />
 		}
-		var play_symb = <span>&#9658;</span>
+		var play_symb = <span>&#9658;</span>;
 		if (oplayer.is_playing) {
-			if (oplayer.episode.guid == episode.guid) {
+			if (oplayer.episode.guid === episode.guid) {
 				play_symb = <span>&#10073;&#10073;</span>
 				if (!playback_loaded) {			
 					play_symb = <span>?</span>
 				}
 			}
-		} 
-		var d = new Date(episode.pubDate)
-		var dstr = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate()
+		}
+
+		const date = moment(episode.pubDate).fromNow();
 		return (
             <div className="home-player">
 				<div className="home-player-card">
-						<div className="home-player-title"><a className="home-player-link" href={episode.link}>{episode.title}</a></div>
-						<p>{dstr}</p>
+						<div className="home-player-title"><Link className="home-player-link" to={episode.link}>{episode.title}</Link></div>
+						<p>{date}</p>
 						<button className="episode-button" onClick={this.onClick.bind(this, episode)}>{play_symb}</button>
 				</div>
             </div>
