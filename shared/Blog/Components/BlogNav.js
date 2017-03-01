c05c84fa47f11d70dae87504319a0ded7b17a175
Gleb L <lozhkovoygleb@icloud.com>
2017-03-01 22:16:53 +0200

    Layout components, blog redesign (#70)

--- shared/Blog/Components/BlogNav.js
+++ shared/Blog/Components/BlogNav.js
@@ -1,37 +1,28 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { Link } from 'react-router'
+import React, { PropTypes } from "react"
 
-import BlogLink from './BlogLink'
+import BlogLink from './BlogLink';
+import BLOGS_NAV_MAP from '../../Blog/Constants/navMap';
 
-export default class BlogNav extends React.Component {
-	
-	render() {
-		const { onClick, activeFolder } = this.props;
-
-		var folders = this.props.folders
-		var pathname = this.props.pathname
-		var up = ".."
-		if (folders == undefined) {
-			return <div></div>
-		}
-		return (
-			<div className="blog-nav">
-				<div className="blog-categories">
-					<div className="blog-link-container">
-						<BlogLink onClick={onClick} active="/blog" to="/blog">{up}</BlogLink>
-					</div>
-					{folders.map(function(folder) {
-						const path = "/blog/" + folder + "/"
-						return (
-							<div key={path} className="blog-link-container">
-								<BlogLink active={pathname} to={path} onClick={onClick} isActive={activeFolder == folder}>{folder}</BlogLink>
-							</div>
-						)
-					})}
-				</div>
-				<div className="clear"></div>
+export const BlogNav = ({folders=[], pathname, up='All', onClick, activeFolder}) => (
+	<div className="blog-nav">
+		<div className="blog-categories">
+			<div className="blog-link-container">
+				<BlogLink onClick={onClick} active="/blog" to="/blog" isActive={activeFolder === up}>{up}</BlogLink>
 			</div>
-		)
-	}
-}
+            {folders.map(function(folder) {
+                const path = "/blog/" + folder + "/";
+                return (
+					<div key={path} className="blog-link-container">
+						<BlogLink active={pathname} to={path} onClick={onClick} isActive={activeFolder === folder}>{BLOGS_NAV_MAP[folder]}</BlogLink>
+					</div>
+                )
+            })}
+		</div>
+	</div>
+);
+
+BlogNav.propTypes = {
+	folders: PropTypes.array,
+};
+
+export default BlogNav;
