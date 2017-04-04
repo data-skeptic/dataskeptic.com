30433c8439e56e3cc286aebc955e68c5264929e4
glozhk <lozhkovoygleb@Icloud.com>
2017-04-04 19:16:13 +0300

    Added email notification after any error

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ const path = require('path');
 
 const app = require('./server').default;
 
+
+const aws = require('aws-sdk');
+aws.config.loadFromPath('awsconfig.json');
+const ses = new aws.SES({apiVersion: '2010-12-01'});
+
+const onError = (err) => {
+	const subject = 'Error';
+	const from = 'kyle@dataskeptic.com';
+	const body = JSON.stringify(err);
+
+    const email_request = {
+        Source: from,
+        Destination: { ToAddresses: ['kyle@dataskeptic.com'] },
+        Message: {
+            Subject: {
+                Data: subject
+            },
+            Body: {
+                Html: {
+                    Data: body
+                }
+            }
+        }
+    };
+
+    ses.sendEmail(email_request, function(err, data) {
+    	if(err) console.log(err);
+    });
+};
+
 if (fs.existsSync('/ssl/cert.pem')) {
 	const httpsOptions = {
 	  cert: fs.readFileSync('/ssl/cert.pem'),
@@ -24,16 +54,19 @@ if (fs.existsSync('/ssl/cert.pem')) {
 	  });
 
 	server.on('error', (err) => {
-		console.error(err);
+		onError(err);
 	});
 
 	http.createServer(function (req, res) {
 	    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
 	    res.end();
-	}).listen(80);	
+	}).listen(80);
 } else {
 	app.listen(3000, function () {
 		console.log('Server listening on 3000');
 	});
 }
 
+process.on('uncaughtException', function(err) {
+    onError(err);
+});
