22b428c536e60cca138638823ec078501d67f3b0
glozhkovoy <lozhkovoygleb@icloud.com>
2017-05-23 11:00:55 +0100

    Test loading animation

--- shared/Common/Components/Loading.js
+++ shared/Common/Components/Loading.js
@@ -2,14 +2,26 @@ import React from 'react'
 
 const Loading = () => {
 	return (
-		<div className="center">
-			<div className="loading">
-				<p>Loading...</p>
-				<div className="loading-icon-container">
-					<p><img className="loading-icon" src="/img/Loading_icon.gif" /></p>
-				</div>
-			</div>
-		</div>
+		<div class="timeline-wrapper">
+  <div class="timeline-item">
+<p>Test Loading</p>
+    <div class="animated-background facebook">
+      <div class="background-masker header-top"></div>
+      <div class="background-masker header-left"></div>
+      <div class="background-masker header-right"></div>
+      <div class="background-masker header-bottom"></div>
+      <div class="background-masker subheader-left"></div>
+      <div class="background-masker subheader-right"></div>
+      <div class="background-masker subheader-bottom"></div>
+      <div class="background-masker content-top"></div>
+      <div class="background-masker content-first-end"></div>
+      <div class="background-masker content-second-line"></div>
+      <div class="background-masker content-second-end"></div>
+      <div class="background-masker content-third-line"></div>
+      <div class="background-masker content-third-end"></div>
+    </div>
+  </div>
+</div>
 	)
 }
 
