ea392272d85ef200564cfb18e46886eccc503b26
glozhkovoy <Glozhkovoyadminbarfw1995>
2017-06-07 14:37:04 +0300

    Done CRUD for player meta

--- backend/api/v1/player.js
+++ backend/api/v1/player.js
@@ -1,13 +1,55 @@
 const express = require('express');
 const MetaDataService = require('../../modules/Player/Services/MetaDataService');
-var stripe_key = "sk_test_81PZIV6UfHDlapSAkn18bmQi";
-var sp_key = "test_Z_gOWbE8iwjhXf4y4vqizQ";
 
 module.exports = (cache) => {
     const router = express.Router();
-    router.post('/', function (req, res) {
+    router.get('/', function (req, res) {
         MetaDataService
             .getMeta()
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.send(err);
+            })
+    });
+    router.get('/:id', function (req, res) {
+        MetaDataService
+            .getMetaById(req.params.id)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.send(err);
+            })
+    });
+    router.post('/', function (req, res) {
+        MetaDataService
+            .insertMeta(req.body)
+            .then((id) => {
+                MetaDataService
+                    .getMetaById(id)
+                    .then(data => {
+                        res.send(data);
+                    })
+            })
+            .catch((err) => {
+                res.send(err);
+            })
+    });
+    router.put('/:id', function (req, res) {
+        MetaDataService
+            .updateMeta(req.params.id, req.body)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.send(err);
+            })
+    });
+    router.delete('/:id', function (req, res) {
+        MetaDataService
+            .deleteMeta(req.params.id)
             .then((data) => {
                 res.send(data);
             })
--- backend/modules/Player/Services/MetaDataService.js
+++ backend/modules/Player/Services/MetaDataService.js
@@ -1,23 +1,126 @@
 const AWS = require('aws-sdk');
+const uuidV4 = require('uuid/v4');
 const docClient = new AWS.DynamoDB.DocumentClient();
-
+const table_config = require('../../../../dynamoDB_tables.json');
+const tablename = `${table_config.dev.player_metadata}`;
 export const getMeta = () => {
-
+    const params = {
+        TableName : tablename
+    };
+    return new Promise((resolve, reject) => {
+        docClient.scan(params, function (err, data) {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                return reject(err);
+            } else {
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                return resolve(data);
+            }
+        });
+    });
 };
 
-export const getMetaByPos = () => {
+export const getMetaById = (id) => {
+    const params = {
+        TableName: tablename,
+        KeyConditionExpression: "#idd = :idd",
+        ExpressionAttributeNames: {
+            "#idd": "id"
+        },
+        ExpressionAttributeValues: {
+            ":idd": id
+        }
+    };
 
+    return new Promise((resolve, reject) => {
+        docClient.query(params, function (err, data) {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                return reject(err);
+            } else {
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                return resolve(data);
+            }
+        });
+    });
 };
 
-export const insertMeta = () => {
-
+export const insertMeta = (data) => {
+    const idOfNewElement = uuidV4();
+    const params = {
+        TableName: tablename,
+        Item: {
+            "id": idOfNewElement,
+            "duration": data.duration,
+            "position" : data.position,
+            "seek" : data.seek
+        }
+    };
+    return new Promise((resolve, reject) => {
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                return reject(err);
+            } else {
+                console.log("Added item:", JSON.stringify(data));
+                return resolve(idOfNewElement);
+            }
+        });
+    });
 };
 
-export const deleteMeta = () => {
-
+export const deleteMeta = (id) => {
+    const params = {
+        TableName: tablename,
+        Key: {
+            "id": id.toString()
+        }
+    };
+    return new Promise((resolve, reject) => {
+        docClient.delete(params, function (err, data) {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                return reject(err);
+            } else {
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                return resolve(data);
+            }
+        });
+    });
 };
 
-export const updateMeta = () => {
+export const updateMeta = (id, data) => {
 
+    const params = {
+        TableName: tablename,
+        Key: {
+            "id": id
+        },
+        UpdateExpression: "set #dur = :dur, #pos = :pos, #sk = :sk",
+        ExpressionAttributeNames: {
+            "#dur": "duration",
+            "#pos" : "position",
+            "#sk" : "seek"
+        },
+        ExpressionAttributeValues: {
+            ":dur": data.duration,
+            ":pos": data.position,
+            ":sk": data.seek,
+
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+    return new Promise((resolve, reject) => {
+        console.log("Updating the item...");
+        docClient.update(params, function (err, data) {
+            if (err) {
+                console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+                return reject(err);
+            } else {
+                console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+                return resolve(data);
+            }
+        });
+    });
 };
 
