e564cfb23b4ff1a289c853e90d8747b6dc7b246c
Billy Everyteen <kyle@datascience.com>
2017-07-01 13:34:42 -0700

    centralizing configuration

--- server.jsx
+++ server.jsx
@@ -72,7 +72,37 @@ app.use(morgan('combined', {stream: accessLogStream}))
 
 var env = "prod"
 
-aws.config.loadFromPath('config/awsconfig.json')
+var aws_accessKeyId = ""
+var aws_secretAccessKey = ""
+var aws_region = ""
+var stripe_key = "sk_test_81PZIV6UfHDlapSAkn18bmQi"
+var sp_key = "test_Z_gOWbE8iwjhXf4y4vqizQ"
+var slack_key = ""
+
+fs.open("config/config.json", "r", function (error, fd) {
+    var buffer = new Buffer(10000)
+    fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+        var data = buffer.toString("utf8", 0, bytesRead)
+        var c = JSON.parse(data)
+        var env2 = env
+        stripe_key = c[env2]['stripe']
+        sp_key = c[env2]['sp']
+        slack_key = c[env2]['slack']
+        aws_accessKeyId = c[env2]['aws']['accessKeyId']
+        aws_secretAccessKey = c[env2]['aws']['secretAccessKey']
+        aws_region = c[env2]['aws']['region']
+        fs.close(fd)
+    })
+})
+
+aws.config.update(
+    {
+        "accessKeyId": aws_accessKeyId,
+        "secretAccessKey": aws_secretAccessKey,
+        "region": aws_region
+    }
+);
+
 
 if (process.env.NODE_ENV !== 'production') {
     require('./webpack.dev').default(app);
@@ -197,23 +227,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
-var stripe_key = "sk_test_81PZIV6UfHDlapSAkn18bmQi"
-var sp_key = "test_Z_gOWbE8iwjhXf4y4vqizQ"
-var slack_key = ""
-
-fs.open("config/config.json", "r", function (error, fd) {
-    var buffer = new Buffer(10000)
-    fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
-        var data = buffer.toString("utf8", 0, bytesRead)
-        var c = JSON.parse(data)
-        var env2 = env
-        stripe_key = c[env2]['stripe']
-        sp_key = c[env2]['sp']
-        slack_key = c[env2]['slack']
-        fs.close(fd)
-    })
-})
 
 function api_router(req, res) {
     if (req.url.indexOf('/api/slack/join') == 0) {
