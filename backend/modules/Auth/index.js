9afcc16e24e034c3ceb8c376568131a78748577c
glozhkovoy <oleg@thespoon.co>
2017-07-31 17:37:05 +0300

    done google oauth

--- backend/modules/Auth/index.js
+++ backend/modules/Auth/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const LinkedinStrategy = require('./LinkedinStrategy').default
+const GoogleStrategy = require('./GoogleStrategy')();
 const UserServices = require('../Users/Services/UserServices');
 
 export default function ({env}) {
@@ -20,7 +21,8 @@ export default function ({env}) {
         done(user)
     })
 
-    //passport.use(LinkedinStrategy({env}))
+    //passport.use(GoogleStrategy({env}))
+    passport.use(LinkedinStrategy({env}))
     router.use(passport.initialize())
     router.use(passport.session())
 
@@ -64,49 +66,75 @@ export default function ({env}) {
             })
         })(req, res, next)
     })
-
+    // GOOGLE
+    router.all('/auth/login/google', (req, res, next) => {
+        passport.authenticate('google', {
+            scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/userinfo.email']
+        })(req, res, next)
+    });
     // LINKEDIN
     router.all('/auth/login/linkedin', passport.authenticate('linkedin'))
-    router.all('/auth/linkedin/activate', function (req, res){
+    router.all('/auth/linkedin/activate', function (req, res) {
         UserServices
             .changeActiveStatus(req.body)
-            .then(status =>{
+            .then(status => {
                 res.redirect('/');
             })
     });
-    router.get('/auth/linkedin/callback', function (req, res, next) {
 
-        passport.authenticate('linkedin', {
-            failWithError: true,
-            failureFlash: true
-        }, function (err, user, info) {
-            return res.send(user);
-          /*  if (err) {
-
-                return res.status(403).send({message: err})
-            }*/
-
+    router.get('/auth/google/callback', (req, res, next) => {
+        passport.authenticate('google', {failWithError: true}, function (err, user, info) {
+            if (err) {
+                return res.status(403).send({message: err.message})
+            }
             if (!user) {
-
                 return res.status(403).send({message: 'System Error'})
             }
-
             req.logIn(user, err => {
                 if (err) {
-
-                    return res.status(403).send({message: err.message})
+                    return res.send({
+                        success: false,
+                        message: err
+                    })
                 }
 
-
-
             })
-        })(req, res, next)
+        })(req, res, next);
     })
 
-    router.all('/auth/logout', function (req, res, next) {
-        req.logout()
-        res.send({})
-    })
+router.get('/auth/linkedin/callback', function (req, res, next) {
 
-    return router
+    passport.authenticate('linkedin', {
+        failWithError: true,
+        failureFlash: true
+    }, function (err, user, info) {
+        return res.send(user);
+        /*  if (err) {
+
+         return res.status(403).send({message: err})
+         }*/
+
+        if (!user) {
+
+            return res.status(403).send({message: 'System Error'})
+        }
+
+        req.logIn(user, err => {
+            if (err) {
+
+                return res.status(403).send({message: err.message})
+            }
+
+
+        })
+    })(req, res, next)
+})
+
+router.all('/auth/logout', function (req, res, next) {
+    req.logout()
+    res.send({})
+})
+
+return router
 }
