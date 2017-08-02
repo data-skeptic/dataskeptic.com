8e0b0a77e7d108412facbc3d3c52cc7e5bffc249
looch <andrew@thespoon.co>
2017-08-02 13:25:50 +0300

    Add styles for mobile

--- shared/ChatBot/Components/ChatBotModal/ChatBotModal.js
+++ shared/ChatBot/Components/ChatBotModal/ChatBotModal.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const ChatBotModal = ({closeModal}) =>(
-    <div className="chat-bot-modal" >
-            <img className="close-modal" src="img/svg/x.svg" alt="close" onClick={closeModal}/>
-        <iframe className="chat-bot" src='https://webchat.botframework.com/embed/dataskeptic-bot?s=Sb3ZVnvH-KE.cwA.9cQ.YtBXA1S8J_1jFukakBxyO_zBaY7DXUdDG7DCLj5ean8'>
-        </iframe>
+const ChatBotModal = ({closeModal}) => (
+    <div className="chat-bot-modal">
+        <div className="bot-wrapper">
+            <i className="fa fa-times close-modal" aria-hidden="true" onClick={closeModal}></i>
+            <iframe className="chat-bot"
+                    src='https://webchat.botframework.com/embed/dataskeptic-bot?s=Sb3ZVnvH-KE.cwA.9cQ.YtBXA1S8J_1jFukakBxyO_zBaY7DXUdDG7DCLj5ean8'>
+            </iframe>
+        </div>
 
-</div>
+    </div>
 )
 export default ChatBotModal;
