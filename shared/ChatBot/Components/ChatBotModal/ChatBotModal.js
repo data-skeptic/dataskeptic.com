970ac0c674f60717f26e61315b9fedbf113feb70
looch <andrew@thespoon.co>
2017-08-02 12:49:38 +0300

    Add basic style for chat bot window on desktop

--- shared/ChatBot/Components/ChatBotModal/ChatBotModal.js
+++ shared/ChatBot/Components/ChatBotModal/ChatBotModal.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ChatBotModal = ({closeModal}) =>(
-    <div onClick={closeModal}>
-        <iframe src='https://webchat.botframework.com/embed/dataskeptic-bot?s=Sb3ZVnvH-KE.cwA.9cQ.YtBXA1S8J_1jFukakBxyO_zBaY7DXUdDG7DCLj5ean8'>
+    <div className="chat-bot-modal" >
+            <img className="close-modal" src="img/svg/x.svg" alt="close" onClick={closeModal}/>
+        <iframe className="chat-bot" src='https://webchat.botframework.com/embed/dataskeptic-bot?s=Sb3ZVnvH-KE.cwA.9cQ.YtBXA1S8J_1jFukakBxyO_zBaY7DXUdDG7DCLj5ean8'>
         </iframe>
 
 </div>
