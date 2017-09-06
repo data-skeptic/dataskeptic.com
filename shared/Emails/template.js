f8994f36f9efaa1cb5b0446c584c8e4f69e83347
Ilya <iliya.lysenko@thespoon.co>
2017-09-06 13:14:15 +0300

    Created EJS email template

---
+++ shared/Emails/template.js
@@ -0,0 +1,10 @@
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+export const getEmail = (data, type) => {
+    const template = fs.readFileSync(path.resolve( __dirname, "layout.ejs" ), 'utf-8');
+    let email =  ejs.render(template, {...data, type});
+
+    return email;
+}
