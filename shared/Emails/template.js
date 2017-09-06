f9e3ae518716fcc0bbc7bd568381d2d7066e1e5d
Ilya <iliya.lysenko@thespoon.co>
2017-09-06 15:21:19 +0300

    Changed all occurrences of email templates in app

--- shared/Emails/template.js
+++ shared/Emails/template.js
@@ -2,8 +2,9 @@ const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
 
-export const getEmail = (data, type) => {
+export const getEmail = (data, type = '') => {
     const template = fs.readFileSync(path.resolve( __dirname, "layout.ejs" ), 'utf-8');
+
     let email =  ejs.render(template, {...data, type});
 
     return email;
