bff21d10952b72b0590781eb1fe8620c7cf02419
looch <andrew@thespoon.co>
2017-09-11 18:32:20 +0300

    Finished work on Name, Email and confirmPolicy logic

--- shared/Questions/Containers/QuestionsContainer.js
+++ shared/Questions/Containers/QuestionsContainer.js
@@ -1,20 +1,37 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import QuestionForm from '../Forms/QuestionForm'
+import {formValueSelector} from 'redux-form';
+import {connect} from 'react-redux';
 
-class QuestionsContainer extends Component{
-    render(){
-        return(
+class QuestionsContainer extends Component {
+
+    render() {
+        const {confirmPolicy} = this.props;
+        return (
             <div className={`center`}>
                 <h3>Listener Questions</h3>
                 <p>We love hearing from our listeners!
-                    If you have a question about one of our episodes or a general question that's relevant to Data Skeptic, please ask via the in-browser recording system below.
+                    If you have a question about one of our episodes or a general question that's relevant to Data
+                    Skeptic, please ask via the in-browser recording system below.
                     Try to keep your question to 30 seconds or less and make sure your question is a question.</p>
-                <div className={`comment-box-form-container`}>
-                <QuestionForm/>
+                <div>
+                    <QuestionForm
+                        allowSubmit={!!confirmPolicy}
+                        initialValues={{
+                            confirmPolicy:true
+                        }}
+                    />
                 </div>
 
             </div>
         )
     }
 }
-export default QuestionsContainer;
+
+const selector = formValueSelector('question');
+
+export default connect(state => ({
+    confirmPolicy: selector(state, 'confirmPolicy')
+}))
+
+(QuestionsContainer);
--- shared/Questions/Forms/QuestionForm.js
+++ shared/Questions/Forms/QuestionForm.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
+import {Field, reduxForm} from 'redux-form'
 import {UserInfoBox} from "../../Proposals/Components/UserInfoBox/UserInfoBox";
 import FormController from '../../Forms/Components/FormController/FormController'
+import {renderField} from "../../Forms/Components/Field/Field";
+import Recorder, {steps} from '../../Recorder';
 
-const QuestionForm = ({handleSubmit, pristine, reset, submitting}) =>(
+const QuestionForm = ({handleSubmit, pristine, reset, submitting, allowSubmit}) => (
 
-  <FormController name={`question`}>
-        <UserInfoBox />
+    <FormController
+        name={`question`}
+        showSubmit={true}
+        invalid={!allowSubmit}
+        submitValue={`Submit`}
+    >
+        <UserInfoBox/>
+        <Field
+            name={`confirmPolicy`}
+            component={renderField}
+            type={`checkbox`}
+            label={`I authorize Data Skeptic to use my recording in a future episode.
+                   I acknowledge my recording will be associated with whatever I type in the 'Name' blank above.
+                   Feel free to type anonymous if you wish.`}
+        />
+        {!allowSubmit
+            ? <p className={`error`}>Sorry, we require this consent before submitting audio feedback.
+                If this is a concern for you, we recommend sending an email instead.</p>
+            : <div></div>}
     </FormController>
- 
-
 )
+
 export default reduxForm({
     form: 'question' // a unique identifier for this form
 })(QuestionForm)
