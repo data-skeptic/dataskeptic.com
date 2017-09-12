5d4b2a4ee4986d2ce899f38bff3abb878fe4cbb7
Ilya <iliya.lysenko@thespoon.co>
2017-09-12 11:03:30 +0300

    Fix

--- shared/Contacts/Containers/ContactFormContainer/ContactFormContainer.js
+++ shared/Contacts/Containers/ContactFormContainer/ContactFormContainer.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import {getEmail} from '../../../../shared/Emails/template';
 import ContactForm from '../../Components/ContactForm/ContactForm'
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
@@ -20,9 +19,8 @@ class ContactFormContainer extends Component {
         const MAIL_SERVICE_URL = "https://obbec1jy5l.execute-api.us-east-1.amazonaws.com/prod/contact";
         const error = '';
         const data = {name, email, msg: message, error};
-        const msg = getEmail(data, 'contact');
         return axios
-            .post(MAIL_SERVICE_URL, msg);
+            .post(MAIL_SERVICE_URL, data);
     }
 
     render() {
