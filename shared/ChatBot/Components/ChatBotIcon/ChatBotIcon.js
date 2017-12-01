ada1bc7c7214af211321deade033faf2fac58674
Kyle Polich <kyle@datascience.com>
2017-12-01 11:50:23 -0800

    bot rollout

--- shared/ChatBot/Components/ChatBotIcon/ChatBotIcon.js
+++ shared/ChatBot/Components/ChatBotIcon/ChatBotIcon.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-//    <div className="chat-bot-icon" onClick={showModal}></div>
-
 const ChatBotIcon = ({showModal}) =>(
-  <div></div>
+  <div className="chat-bot-icon" onClick={showModal}></div>
 )
 export default ChatBotIcon;
