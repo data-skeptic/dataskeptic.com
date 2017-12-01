4b90e91d11a98b72ffd6b4d48d79651fc5b2167a
Kyle Polich <kyle@datascience.com>
2017-12-01 14:41:09 -0800

    parameterizing bot

--- shared/ChatBot/Components/ChatBotModal/ChatBotModal.js
+++ shared/ChatBot/Components/ChatBotModal/ChatBotModal.js
@@ -4,12 +4,14 @@ var env = "prod"
 const c = require('../../../../config/config.json')
 var bot_embed_code = c[env]['bot']['embed_code']
 
+var src = 'https://webchat.botframework.com/embed/dataskeptic-bot?s=' + bot_embed_code
+
 const ChatBotModal = ({closeModal}) => (
     <div className="chat-bot-modal">
         <div className="bot-wrapper">
             <i className="fa fa-times close-modal" aria-hidden="true" onClick={closeModal}></i>
             <iframe className="chat-bot"
-                    src='https://webchat.botframework.com/embed/dataskeptic-bot?s={bot_embed_code}'>
+                    src={src}>
             </iframe>
         </div>
 
