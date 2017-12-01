87d25dae4300360ad1c3e747b76ebee548e3bfba
Gleb L <glozhkovoy@users.noreply.github.com>
2017-12-01 12:17:32 +0200

    Added blog details page

--- pages/blogs/details.js
+++ pages/blogs/details.js
@@ -3,20 +3,23 @@ import Link from '../../components/Link'
 import Icon from 'react-fontawesome'
 import styled from 'styled-components'
 import Page from '../../hoc/Page'
+import {loadBlogList} from "../../redux/modules/blogReducer";
 
 @Page
-export default class Services extends Component {
+export default class BlogDetails extends Component {
     static async getInitialProps({ store: { dispatch, getState }, query }) {
-        const state = getState()
+        const {slug} = query;
         const promises = []
-
-        await Promise.all(promises)
+         promises.push(dispatch(loadBlogList()))
+         await Promise.all(promises)
     }
 
     render() {
+        const { url: { query: { id } } } = this.props
         return (
             <Container>
-                <Title>Services Page</Title>˚†
+              {id ? <Title>Blog {id} Page</Title> : <h1>No such page</h1>}
+
 
                 <Link href="/">Home</Link>
             </Container>
