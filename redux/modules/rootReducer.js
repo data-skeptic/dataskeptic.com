b142ee1bca552b1e2159ddce4a3b1acb058fd254
Gleb L <lozhkovoygleb@icloud.com>
2017-12-01 15:13:07 +0200

    Add home reducer

--- redux/modules/rootReducer.js
+++ redux/modules/rootReducer.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux'
 import router from './router'
 import {default as blogs} from './blogReducer'
 import {default as podcasts } from './podcastReducer'
+import {default as home} from './homeReducer'
 
 export default combineReducers({
     router,
+    home,
     blogs,
-    podcasts
+    podcasts,
+
 })
