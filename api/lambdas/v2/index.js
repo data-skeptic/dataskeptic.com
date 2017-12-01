933ea29cdf31bb4448ced751511d93a6e676d06c
Gleb L <glozhkovoy@users.noreply.github.com>
2017-12-01 11:22:14 +0200

    Backend fixes

--- api/lambdas/v2/index.js
+++ api/lambdas/v2/index.js
@@ -1,33 +1,39 @@
-import express from 'express'
-import morgan from 'morgan'
-import config from '../../../config'
+import express from "express";
+import morgan from "morgan";
+import config from "../../../config";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 
 async function run() {
-    app.use(morgan('combined'))
+  app.use(morgan("combined"));
+  app.use(bodyParser.json());
 
-    app.use('/blogs', require('./blogs'))
-    app.use('/episodes', require('./episodes'))
+  app.use((req, res, next) => {
+    next();
+  });
+  app.get('/', (req,res) => {res.send({status: 'OK'})})
+  app.use("/blogs", require("./blogs"));
+  app.use("/episodes", require("./episodes"));
 
-    if (config.apiPort) {
-        app.listen(config.apiPort, err => {
-            if (err) {
-                console.error(err) // eslint-disable-line no-console
-            }
-            console.info('----\n==> 🌎  API is running on port %s', config.apiPort) // eslint-disable-line no-console
-            console.info(
-                '==> 💻  Send requests to http://%s:%s',
-                config.apiHost,
-                config.apiPort
-            ) // eslint-disable-line no-console
-        })
-    } else {
-        console.error(
-            '==>     ERROR: No PORT environment variable has been specified'
-        ) // eslint-disable-line no-console
-        console.log('config', config)
-    }
+  if (config.apiPort) {
+    app.listen(config.apiPort, err => {
+      if (err) {
+        console.error(err); // eslint-disable-line no-console
+      }
+      console.info("----\n==> 🌎  API is running on port %s", config.apiPort); // eslint-disable-line no-console
+      console.info(
+        "==> 💻  Send requests to http://%s:%s",
+        config.apiHost,
+        config.apiPort
+      ); // eslint-disable-line no-console
+    });
+  } else {
+    console.error(
+      "==>     ERROR: No PORT environment variable has been specified"
+    ); // eslint-disable-line no-console
+    console.log("config", config);
+  }
 }
 
-run()
+run();
