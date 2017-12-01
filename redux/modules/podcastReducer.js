13a556a1d61762bf5f614efe9f5d232053968bec
Gleb L <lozhkovoygleb@icloud.com>
2017-12-01 13:41:13 +0200

    Add podcast reducer

---
+++ redux/modules/podcastReducer.js
@@ -0,0 +1,45 @@
+const LOAD_EPISODES = 'LOAD_EPISODES'
+const LOAD_EPISODES_SUCCESS = 'LOAD_EPISODES_SUCCESS'
+const LOAD_EPISODES_FAIL = 'LOAD_EPISODES_FAIL'
+
+
+const initialState = {
+    loading:false,
+    error:false,
+    episodes: null,
+    single: null,
+    years:null,
+}
+
+export default function reducer(state = initialState,
+                                action = {}) {
+    switch (action.type) {
+        case LOAD_EPISODES:
+            return{
+                ...state,
+                loading:true
+            }
+        case LOAD_EPISODES_SUCCESS:
+            return{
+                ...state,
+                episodes:action.result
+            }
+        default:
+            return state
+    }
+}
+
+
+export const loadEpisodesList = () => ({
+    types: [LOAD_EPISODES, LOAD_EPISODES_SUCCESS, LOAD_EPISODES_FAIL],
+    promise: client => client.get('/episodes')
+})
+
+
+//Selectors
+
+export const getBlogList = state => state.blogs && state.blogs.list
+
+export const getSingle = state => state.blogs && state.blogs.single
+
+export const getCategories = state => state.blogs && state.blogs.categories
