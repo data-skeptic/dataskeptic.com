5a2ea390e6a7e5ad9663f6485225df91173261f6
glozhkovoy <Glozhkovoyadminbarfw1995>
2017-12-04 13:26:41 +0200

    added prettyname for categories

--- api/lambdas/v2/routes/categories.js
+++ api/lambdas/v2/routes/categories.js
@@ -6,49 +6,64 @@ const router = express.Router()
 router.get('/', (req, res) => {
     const list = [{
         "id":1,
-        "label":"All"
+        "label":"All",
+        "prettyname":"all"
     }, {
         "id":2,
-        "label":"Book Reviews"
+        "label":"Book Reviews",
+        "prettyname":"book-reviews"
     }, {
         "id":3,
-        "label":"Data Collection"
+        "label":"Data Collection",
+        "prettyname":"data-collection"
     }, {
         "id":4,
-        "label":"Episodes"
+        "label":"Episodes",
+        "prettyname":"episodes"
     }, {
         "id":5,
-        "label":"Infrastructure"
+        "label":"Infrastructure",
+        "prettyname":"infrastructure"
     }, {
         "id":6,
-        "label":"Meta"
+        "label":"Meta",
+        "prettyname":"meta"
     }, {
         "id":7,
-        "label":"Methods"
+        "label":"Methods",
+        "prettyname":"methods"
     }, {
         "id":8,
-        "label":"News"
+        "label":"News",
+        "prettyname":"news"
     }, {
         "id":9,
-        "label":"Open House"
+        "label":"Open House",
+        "prettyname":"open-house"
     }, {
         "id":10,
-        "label":"Opinions"
+        "label":"Opinions",
+        "prettyname":"opinions"
     }, {
         "id":11,
-        "label":"Podcasting"
+        "label":"Podcasting",
+        "prettyname":"podcasting"
     }, {
         "id":12,
-        "label":"Projects"
+        "label":"Projects",
+        "prettyname":"projects"
     }, {
         "id":13,
-        "label":"Skeptical Analysis"
+        "label":"Skeptical Analysis",
+        "prettyname":"skeptical-analysis"
     }, {
         "id":14,
-        "label":"Tools-and-techniques"
+        "label":"Tools-and-techniques",
+        "prettyname":"tools-and-techniques"
     }, {
         "id":15,
-        "label":"Transcripts"
+        "label":"Transcripts",
+        "prettyname":"transcripts"
     }]
 
     res.send(list)
@@ -57,7 +72,8 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const post = {
         "id":1,
-        "label":"All"
+        "label":"All",
+        "prettyname":"all"
     }
 
     res.send(post)
