8412a3a10e83da7e11b058b140fae2c19b3964c1
Gleb L <lozhkovoygleb@icloud.com>
2017-12-08 14:50:23 +0200

    work on input styles

--- modules/Forms/ContactForm.js
+++ modules/Forms/ContactForm.js
@@ -5,20 +5,35 @@ import {Input} from "./Input";
 
 const ContactForm = ({handleSubmit, reset, submitting, pristine, values}) => (
     <form onSubmit={handleSubmit}>
-        <Field name="fullName"
+        <ContactInput name="fullName"
                component={Input}
                type="text"
+               label="Name"
                placeholder="Name"
         />
-        <Field name="email"
+        <ContactInputnpm name="email"
                component={Input}
                type="email"
+               label="Email"
                placeholder="Email"
         />
-        <Field name="message"
-               component={Input}
-               textarea
+        <Field
+            name="message"
+            component={Input}
+            textarea
+            label="Message"
         />
+            <button type="submit">Submit</button>
     </form>
 )
 export default ContactForm
+
+const ContactInput = styled(Field)`
+  input{
+    padding: 12px 16px;
+    background: #fff;
+    border: 1px solid #d7d9d9;
+    border-radius: 4px;
+    width: 100%;
+  }
+`
