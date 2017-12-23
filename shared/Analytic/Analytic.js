a9ebc4e2c14cb7d9df10c7c7d0c9e947e0501df6
glozhkovoy <lozhkovoygleb@icloud.com>
2017-12-23 14:59:30 +0200

    Analytic component. CMS less file

---
+++ shared/Analytic/Analytic.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Analytic extends Component {
+
+    render() {
+        return (
+            <div>
+                Analytic
+            </div>
+    }
+
+}
