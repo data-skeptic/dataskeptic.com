3e92e1db43f419f11794c3a890314b884216c260
glozhkovoy <lozhkovoygleb@icloud.com>
2017-12-23 18:35:27 +0200

    Analytics page

---
+++ shared/components/Analytics.js
@@ -0,0 +1,49 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import {changePageTitle} from '../Layout/Actions/LayoutActions';
+import Analytic from "../Analytic/Analytic";
+
+class AnalyticsPage extends Component {
+    constructor() {
+        super();
+    }
+
+    componentWillMount() {
+        const {title} = AnalyticsPage.getPageMeta();
+        this.props.changePageTitle(title);
+    }
+
+    componentDidMount() {
+        if (!this.props.loggedIn) {
+            window.location.href = '/login'
+        }
+    }
+
+    static getPageMeta() {
+        return {
+            title: 'Analytics | Data Skeptic'
+        }
+    }
+
+    render() {
+        const { user, loggedIn } = this.props
+
+        return loggedIn && (
+            <div className="center">
+                <Analytic />
+            </div>
+        )
+    }
+}
+
+export default connect(
+    (state) => ({
+        user: state.auth.getIn(['user']).toJS(),
+        loggedIn: state.auth.getIn(['loggedIn']),
+    }),
+    (dispatch) => bindActionCreators({
+        changePageTitle
+    }, dispatch)
+)(AnalyticsPage);
