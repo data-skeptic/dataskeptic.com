c3afcfeb26686315dbe63701bdc032c011a5af0a
Kyle Polich <kyle@datascience.com>
2018-01-10 11:57:16 -0800

    improve admin ui label

--- shared/components/admin/RelatedContent.js
+++ shared/components/admin/RelatedContent.js
@@ -29,7 +29,7 @@ class RelatedContent extends React.Component {
       } else if (val == "mp3") {
         me.setState({"lbl2": "Media URL", "lbl3": "Title of recording", "lbl4": "Description", "dest": "https://dataskeptic.com/blog/", "selected": true})
       } else if (val == "person") {
-        me.setState({"lbl2": "Leave blank", "lbl3": "Guest Name", "lbl4": "Guest Bio", "dest": "", "selected": true})
+        me.setState({"lbl2": "IMG URL", "lbl3": "Guest Name", "lbl4": "Guest Bio", "dest": "", "selected": true})
       } else if (val == "homepage-image") {
         me.setState({"lbl2": "Image url (600x250px)", "lbl3": "Alt text", "lbl4": "Leave Blank", "dest": "", "selected": true})
       } else if (val == "blog-header-img") {
