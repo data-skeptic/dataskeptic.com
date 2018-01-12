2614a9287225126880f7bd5b5667fcfb279dc732
urchindev <mario.as.dan@gmail.com>
2018-01-12 09:01:33 -0800

    completed the ui for home page

--- shared/Cart/Components/CartMenu.js
+++ shared/Cart/Components/CartMenu.js
@@ -5,7 +5,7 @@ import CartLink from './CartLink'
 
 export const CartMenu = ({pathname, cartItemsCount, cartClick, cartButton = true, itemClick}) => (
     <div className="nav">
-        <NavLink active={pathname} to="/members" onClick={itemClick}><span className="membership-link">Membership</span></NavLink>
+        <NavLink active={pathname} to="/members" onClick={itemClick}><span className="membership-link">Sponsor</span></NavLink>
         <NavLink active={pathname} to="/store" onClick={itemClick}>Store</NavLink>
         { cartButton ?
         <CartLink itemCount={cartItemsCount} onClick={cartClick} itemCount={cartItemsCount}/>
