324b0194fd4d3695fa54c32bebba3c3e47b8afa9
glozhkovoy <lozhkovoygleb@icloud.com>
2018-01-24 15:30:33 +0200

    Hide `Membership` menu item on login

--- shared/Cart/Components/CartMenu.js
+++ shared/Cart/Components/CartMenu.js
@@ -3,10 +3,11 @@ import React from 'react'
 import NavLink from '../../components/NavLink'
 import CartLink from './CartLink'
 
-export const CartMenu = ({pathname, cartItemsCount, cartClick, cartButton = true, itemClick}) => (
+export const CartMenu = ({pathname, cartItemsCount, cartClick, cartButton = true, itemClick, loggedIn}) => (
     <div className="nav">
-        <NavLink active={pathname} to="/members" onClick={itemClick}><span className="membership-link">Membership</span></NavLink>
+        {!loggedIn && <NavLink active={pathname} to="/members" onClick={itemClick}><span className="membership-link">Membership</span></NavLink>}
         <NavLink active={pathname} to="/store" onClick={itemClick}>Store</NavLink>
+
         { cartButton ?
         <CartLink itemCount={cartItemsCount} onClick={cartClick} itemCount={cartItemsCount}/>
         : null }
