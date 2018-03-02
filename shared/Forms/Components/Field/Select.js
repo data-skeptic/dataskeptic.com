96d936a8f1dfd655f6b383fd054c1e7433545bd0
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-03-02 15:48:26 +0200

    Add job form validation

--- shared/Forms/Components/Field/Select.js
+++ shared/Forms/Components/Field/Select.js
@@ -10,7 +10,7 @@ export const renderSelect = ({
         <div className={`field-container ${fieldWrapperClasses}`}>
             <div className={`field-label ${labelWrapperClasses}`}>{label}&nbsp;{required ? <span className="required">*</span> : null}</div>
             <div className={`field-input ${inputWrapperStyles}`}>
-	              <select {...input}>
+	              <select {...input} className={`${inputWrapperStyles} ${touched && invalid ? 'has-danger' : ''}`}>
                   {options.map(op => <option value={op.value} key={op.value}>{op.label}</option>)}
                 </select>
                 <p className="error-message">{touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}</p>
--- shared/Jobs/Forms/AddJob.js
+++ shared/Jobs/Forms/AddJob.js
@@ -3,8 +3,48 @@ import { Field, reduxForm } from "redux-form"
 import FormController from "../../Forms/Components/FormController/FormController"
 import { renderField, renderSelect, renderZip } from "../../Forms/Components/Field"
 
+const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)
+
+const urlFields = ['company_url', 'company_logo', 'url']
+
 const validate = values => {
   let errors = {}
+
+  urlFields.forEach((field) => {
+    const val = values[field] || ''
+
+	  if (!val.match(urlRegex)) {
+		  errors[field] = 'Invalid URL'
+	  }
+  })
+
+	if (!values['title']) {
+    errors['title'] = 'Cannot be empty.'
+  }
+
+	if (!values['created_at']) {
+    errors['created_at'] = 'Cannot be empty.'
+  }
+
+	if (!values['location']) {
+    errors['location'] = 'Cannot be empty.'
+  }
+
+	if (!values['type']) {
+    errors['type'] = 'Cannot be empty.'
+  }
+
+	if (!values['company']) {
+    errors['company'] = 'Cannot be empty.'
+  }
+
+	if (!values['company_url']) {
+    errors['company_url'] = 'Cannot be empty.'
+  }
+
+	if (!values['company_logo']) {
+    errors['company_logo'] = 'Cannot be empty.'
+  }
 
   return errors
 }
