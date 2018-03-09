dc2143d30bbfe0db888cfe16a283429e65aa639e
jing chen <jing@dataskeptic.com>
2018-03-08 17:27:19 -0800

    resume issue

--- etl/resume-parser/index.js
+++ etl/resume-parser/index.js
@@ -38,15 +38,19 @@ String.prototype.replaceAll = function (find, replace) {
     return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
 };
 
+function get_local_filename_from_s3_key(Key) {
+	var filename = Key.replaceAll('/', '_')
+	console.log(filename)
+	return filename
+}
+
 function get_pdf_from_s3_promise(Bucket, Key) {
 	return new Promise(function(resolve, reject) {
 		var params = {Bucket, Key}
-		var filename = Key.replaceAll('/', '_')
-		console.log(filename)
+		var filename = get_local_filename_from_s3_key(Key)
 		s3.getObject(params, function(err, data) {
 			if (err) {
 				reject(err)
-				fs.unlink(filename)
 			} else {
 				var file = fs.createWriteStream(filename)
 				var buff = data.Body
@@ -62,24 +66,27 @@ function convert_pdf_to_text_promise(pdf_absfile) {
 	return new Promise(function(resolve, reject) {
 		var option = {from: 0, to: 10}
 		var filename = pdf_absfile + ".txt"
-		console.log(pdf_absfile)
+		const testFolder = './';
 		pdfUtil.pdfToText(pdf_absfile, option, function(err, data) {
 			console.log(pdf_absfile)
 		  	if (err) reject(err)
+		  	console.log('10')
 			fs.writeFile(filename, data, function(err) {
-				fs.unlink(pdf_absfile)
 			    if (err) {
+			    	console.log('12')
 			    	reject(err)
 			    } else {
-			    	resolve(filename)
+			    	console.log(filename)
+			    	resolve(filename, pdf_absfile)
 			    }
-			});
+			})
 		});		
 	})
 }
 
 function move_text_to_s3_promise(Bucket, Key, txt_file) {
 	return new Promise(function(resolve, reject) {
+		console.log('move_text_to_s3_promise!!!')
 		var Body = fs.createReadStream( txt_file )
 		var opts = {
 			Bucket,
@@ -89,7 +96,6 @@ function move_text_to_s3_promise(Bucket, Key, txt_file) {
 			Body
 		}
 		s3.putObject(opts, function(err, data) {
-			fs.unlink(txt_file)
 	 		if (err) {	
 	 			reject(err)
 	 		} else {
@@ -133,46 +139,100 @@ function save_parse_to_es(parse_result) {
 		        awards		   : parse_result.awards
             }
         })
-        console.log(parse_result)
 		var result = {"success": false, "msg": "not implemented yet"}
 		resolve(result)
 	})
 }
 
-function process_file(bucket, s3key) {
-	get_pdf_from_s3_promise(bucket, s3key)
+
+
+function cleanup(s3key) {
+	console.log('function run')
+	var filename = get_local_filename_from_s3_key(s3key) 
+	console.log('***'+filename)
+	if (fs.existsSync(filename)) {
+		fs.unlinkSync(filename)
+	}
+	var filename2 = filename + '.txt'
+	console.log('???'+filename2)
+	if (fs.existsSync(filename2)) {
+		console.log('<<<<<<')
+		fs.unlinkSync(filename2)
+	}	
+}
+
+
+
+var p = new Promise(function(resolve, reject) {
+	return get_pdf_from_s3_promise(bucket, s3key)
 		.then(function (local_file) {
+			console.log('get_pdf_from_s3_promise')
 			return convert_pdf_to_text_promise(local_file)
 		})
-		.then(function(txt_file) {
+		.then(function(txt_file, pdf_absfile) {
+			console.log('convert_pdf_to_text_promise')
 			return move_text_to_s3_promise(bucket, s3path + 'text/' + txt_file, txt_file)
 		})
 		.then(function(data) {
 			var txt_file = data['filename']
+			console.log('move_text_to_s3_promise')
 			return parse_text_promise(bucket, s3path, txt_file)
 		})
 		.then(function(parse_result) {
+			console.log('parse_text_promise')
 			return save_parse_to_es(parse_result)
 		})
 		.then(function(result) {
+			console.log('save_parse_to_es')
 			console.log(result)
+			resolve(s3key)
 		})
 		.catch(function(err) {
 			console.log("error!!!")
 			console.log(err)
+			reject(s3key) 
 		})
+})
 
-}
 
 
+
+
+
+
+
+	
+
+
+p.then(cleanup).catch(cleanup)
+
+
+
+
+
+
+
+//var p = process_file(bucket, 'resumes/unsourced/00a7d5ee.pdf')
+/*
+p.then(function(s3key){
+	console.log('done!!!!!!!')
+	cleanup(s3key)
+})
+.then(function(){
+	console.log('done')
+})
+*/
+
+
+
+/*
 s3.listObjects(params, function (err, data) {
  if(err)throw err;
- console.log(data)
  var IsTruncated = data['IsTruncated']
  if (IsTruncated) {
  	console.log('err!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
  }
- var Contents = data['CommonPrefixes'].slice(76,77)
+ var Contents = data['CommonPrefixes'].slice(74,75)
  for (var item of Contents) {
  	var Prefix = item['Prefix']
  	var n = Prefix.length
@@ -184,10 +244,22 @@ s3.listObjects(params, function (err, data) {
  	}
  }
 });
+*/
+
+//process_file(bucket, 'resumes/unsourced/ALIREZA_ALIAMIRI.pdf') 
+
+/*
+try{
+
+process_file(bucket, 'resumes/unsourced/00a7d5ee.pdf')
+
+}catch(e){
+
+console.log('error');
+}
 
 
-
-
+*/
 
 // If exists, delete local files
 // Change ACL to private
