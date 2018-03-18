4594315edf3ea25065eb73812c462864dfcb55bd
Kyle Polich <kyle@dataskeptic.com>
2018-03-18 00:02:53 -0700

    missing page

---
+++ shared/components/drip/ThankYou.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+class DripThankYou extends Component {
+  render() {
+    return (
+    	<div className="center">
+    		<h2>Thanks for your feedback!</h2>
+    		<p>As you can imagine, we're collecting your feedback as training data.</p>
+    		<p>In time, this page will be improved to provide you personalized feedback about how your rating compares to others.  So keep waiting and rating, as we build that out.</p>
+    	</div>
+    )
+  }
+}
+
+export default connect(
+  state => ({}),
+  dispatch => bindActionCreators({}, dispatch)
+)(DripThankYou)
