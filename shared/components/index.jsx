33227e35dc586a4ef9e853c100d90fcc9d7508a7
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-04-02 10:53:05 +0300

    Updated inactive message logic

--- shared/components/index.jsx
+++ shared/components/index.jsx
@@ -292,7 +292,7 @@ class MainView extends React.Component {
           messages={this.state.messages}
           onMessage={this.onMessage}
           onInactivity={this.onInactivity}
-          inactivityDelay={1000 * 60 * 5}
+          inactivityDelay={3000}
         />
       </div>
     )
