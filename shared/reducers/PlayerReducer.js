5d50fcc511b8fc9027ec20d9764fa7f0d254ca44
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-04-02 14:58:48 +0300

    Initial localStorage player memorization

--- shared/reducers/PlayerReducer.js
+++ shared/reducers/PlayerReducer.js
@@ -12,11 +12,86 @@ const init = {
   episode: {}
 }
 
+const IS_CLIENT = (() => {
+  let isDefined = false
+  try {
+    window
+    isDefined = true
+  } catch (x) {}
+  return isDefined
+})()
+
+const normalizeState = ({ is_playing, position, episode, volume }) => ({
+  is_playing,
+  position,
+  episode,
+  volume
+})
+
+const getKey = key => {
+  let data = {}
+  if (!IS_CLIENT) return data
+  try {
+    data = JSON.parse(localStorage.getItem(key))
+  } catch (err) {}
+  return data
+}
+
+const PLAYER_PLAYING_KEY = 'player_playing'
+const PLAYER_META_KEY = 'player_meta'
+
+const savePlaying = meta => {
+  if (!IS_CLIENT) return
+
+  const data = {
+    ...meta.episode
+  }
+
+  localStorage.setItem(PLAYER_PLAYING_KEY, JSON.stringify(data))
+}
+
+const savePlayingMeta = ({ position, has_shown, is_playing, volume }) => {
+  if (!IS_CLIENT) return null
+
+  const data = {
+    position,
+    has_shown,
+    is_playing,
+    volume
+  }
+
+  localStorage.setItem(PLAYER_META_KEY, JSON.stringify(data))
+}
+
+const removePlaying = () => {
+  localStorage.removeItem(PLAYER_PLAYING_KEY)
+  localStorage.removeItem(PLAYER_META_KEY)
+}
+
+const getCachePlaying = () => {
+  const { position, is_playing, volume } = getKey(PLAYER_META_KEY)
+
+  const episode = getKey(PLAYER_PLAYING_KEY)
+
+  return {
+    position,
+    is_playing,
+    volume,
+    episode
+  }
+}
+
 const defaultState = Immutable.fromJS(init)
 
 export default function PlayerReducer(state = defaultState, action) {
   var nstate = state.toJS()
   switch (action.type) {
+    case '@@redux/INIT':
+      nstate = {
+        ...nstate,
+        ...getCachePlaying()
+      }
+      break
     case 'PLAY_EPISODE_FROM_GUID':
       // This is used by the bot
       console.log('PLAY_EPISODE_FROM_GUID')
@@ -79,34 +154,48 @@ export default function PlayerReducer(state = defaultState, action) {
           nstate.is_playing = true
         }
       }
+      savePlaying(nstate)
+      savePlayingMeta(nstate)
       break
+
     case 'STOP_PLAYBACK':
       nstate.is_player = false
+      savePlayingMeta(nstate)
       break
 
     case 'STOP_PLAYING':
       nstate.is_playing = false
+      savePlayingMeta(normalizeState(nstate))
       break
 
     case 'RESUME_PLAYING':
       nstate.is_playing = true
+      savePlayingMeta(nstate)
       break
 
     case 'PLAYBACK_LOADED':
       nstate.playback_loaded = action.payload
+      savePlayingMeta(nstate)
       break
     case 'PROGRESS_UPDATE':
       var pos = action.payload
       nstate.position = pos
+      savePlayingMeta(nstate)
       break
     case 'PLAYER_SEEK':
       var pos = action.payload
       nstate.position = pos
       nstate.position_updated = true
+      savePlayingMeta(nstate)
       break
     case 'SEEK_SET':
       nstate.position_updated = false
       break
+
+    case 'MARK_AS_PLAYED':
+      removePlaying(nstate)
+      break
   }
+
   return Immutable.fromJS(nstate)
 }
