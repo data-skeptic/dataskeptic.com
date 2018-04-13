1f8b07e68c1cb355fd89a027cde354f04a27f5dd
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-04-13 14:30:03 +0300

    New podcasts api endpoint

---
+++ backend/api/v1/podcasts.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const axios = require('axios')
+
+const c = require('../../../config/config.json')
+const env = process.env.NODE_ENV === 'dev' ? 'dev' : 'prod'
+const base_api = c[env]['base_api'] + env
+
+const getEpisodes = (limit, offset) => {
+  const url =
+    base_api + `/podcast/episodes/list?limit=${limit}&offset=${offset}`
+  return axios.get(url).then(res => res.data)
+}
+
+module.exports = cache => {
+  const router = express.Router()
+
+  router.get('/', (req, res) => {
+    const { limit = 10, offset = 0 } = req.query
+
+    return getEpisodes(limit, offset)
+      .then(list =>
+        res.send({
+          success: true,
+          list
+        })
+      )
+      .catch(error =>
+        res.status(500).send({ success: false, error: error.data.errorMessage })
+      )
+  })
+
+  return router
+}
