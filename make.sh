08ec9959661a8bbcd64aba82d285f1f9964dbded
Ubuntu <ubuntu@ip-172-26-11-11.ec2.internal>
2018-04-22 02:40:39 +0000

    make update

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
-#git pull
-#npm i
+echo "Going to make and tag as ---> $1 <----"
+git pull
+read -p "Press enter to continue"
+npm i
+read -p "Press enter to continue"
 #npm run build
-#docker build -t kylepolich/dataskeptic.com .
-#docker tag kylepolich/dataskeptic.com kylepolich/dataskeptic.com:2
-#tail -1 Dockerfile
-
+read -p "Press enter to continue"
+docker build -t $1 .
+read -p "Press enter to continue"
 docker kill $(docker ps | grep -v "CONTAINER ID" | awk '{print $1}')
-docker run -i -t -d -p 443:443 -p 80:80 -p 9001:9001 ds75
+docker run -i -t -d -p 443:443 -p 80:80 -p 9001:9001 $1
 docker logs -f $(docker ps | grep -v "CONTAINER ID" | awk '{print $1}')
 
