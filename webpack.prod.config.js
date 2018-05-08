3c34047930309c76d67f8facf39a40f7245fe238
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-05-08 19:24:40 +0300

    Local docker build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,7 +4,16 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 var extractLESS = new ExtractTextPlugin('[name].css')
 
-var dotenv = require('dotenv').config({ path: __dirname + '/.env' })
+const env = {
+  'process.env.NODE_ENV': JSON.stringify(process.env['NODE_ENV']),
+  'process.env.BASE_API': JSON.stringify(process.env['BASE_API']),
+  'process.env.AWS_ACCESS_KEY_ID': JSON.stringify(process.env['AWS_ACCESS_KEY_ID']),
+  'process.env.AWS_SECRET_ACCESS_KEY': JSON.stringify(process.env['AWS_SECRET_ACCESS_KEY']),
+  'process.env.AWS_REGION': JSON.stringify(process.env['AWS_REGION']),
+  'process.env.AWS_CONFIG_S3_BUCKET': JSON.stringify(process.env['AWS_CONFIG_S3_BUCKET']),
+}
+
+console.dir(env)
 
 module.exports = {
   entry: ['babel-polyfill', './client'],
@@ -32,15 +41,7 @@ module.exports = {
   plugins: [
     extractLESS,
 
-    // expose only secure variables to the client  
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(dotenv.parsed['NODE_ENV']),
-      'process.env.BASE_API': JSON.stringify(dotenv.parsed['BASE_API']),
-      'process.env.AWS_ACCESS_KEY_ID': JSON.stringify(dotenv.parsed['AWS_ACCESS_KEY_ID']),
-      'process.env.AWS_SECRET_ACCESS_KEY': JSON.stringify(dotenv.parsed['AWS_SECRET_ACCESS_KEY']),
-      'process.env.AWS_REGION': JSON.stringify(dotenv.parsed['AWS_REGION']),
-      'process.env.AWS_CONFIG_S3_BUCKET': JSON.stringify(dotenv.parsed['AWS_CONFIG_S3_BUCKET']),
-    }),
+    new webpack.DefinePlugin(env),
 
     new webpack.optimize.UglifyJsPlugin({
       compress: {
