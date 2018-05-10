6ed545bc009efc75dde85330563e2ed8c0fe438f
Kyle Polich <kyle@dataskeptic.com>
2018-05-10 00:11:37 -0700

    shell scripts

---
+++ startup.sh
@@ -0,0 +1,17 @@
+echo "Starting!"
+cd /usr/src/app/
+if [ -n $NODE_ENV ]
+then
+  if [ $NODE_ENV == "dev" ]
+  then
+    echo "Starting in DEV mode"
+    npm run dev
+    exit
+ else
+    echo "Starting in PROD mode"
+    npm run start
+    exit
+  fi
+else
+  echo -e "NODE_ENV not set.\n"
+fi
