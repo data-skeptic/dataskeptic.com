e88f91b7c9538aee901bbbfa7bfdf9977fcb4015
Kyle Polich <kyle@dataskeptic.com>
2018-05-10 00:11:17 -0700

    docker setup

--- run-local.sh
+++
@@ -1,1 +0,0 @@
-docker run -t -i -p 3000:3000 -p 4430:4430 -p 9001:9001 --env-file .env dataskeptic:latest
--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -t -i -p 80:3000 -p 443:4430 -p 9001:9001 --env-file .env dataskeptic:latest
