c214b5ed852649cb398378349ae0c75029d2b2f8
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-05-16 13:59:51 +0300

    Moved to the new routing config

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router'
 import ReactGA from 'react-ga'
 
 import About from 'components/About'
@@ -82,7 +82,138 @@ export default (
   <Router>
     <Route path="/" name="app" component={App} onEnter={loadData}>
       <IndexRoute component={Home} />
+
+      <Route path="/loading" component={Loading} />
+
+      <Route path="/lightsout" component={LightsOut} />
+
+      <Route path="/members" component={Membership} />
+
+      <Route path="/about" component={About} />
+
+      <Route path="/advertising" component={Advertising} />
+
+      <Route path="/coaching" component={Coaching} />
+
+      <Route path="/coaching2" component={Coaching2} />
+
+      <Route path="/coaching-weekly" component={CoachingBiWeekly} />
+
+      <Route path="/coaching-biweekly" component={CoachingBiWeekly} />
+
+      <Route path="/checkout" component={Checkout} />
+
+      <Route path="/thank-you" component={CheckoutThankYouPage} />
+
+      <Route path="/contact-us" component={ContactUs} />
+
+      <Route path="/careers/city/:id" component={CityCareers} />
+
+      <Route path="/careers" component={Careers} />
+
+      <Route path="/careers/jobs/add" component={PublicAddJob} />
+
+      <Route path="/blog" component={BlogRouter} />
+
+      <Route path="/podcast*" component={Podcast} />
+
+      <Route
+        path="/services*"
+        component={props => <Services foo="value" bar="v2" location={props} />}
+      />
+
+      <Route path="/store" component={Store} />
+
+      <Route path="/press" component={Press} />
+
+      <Route path="/projects" component={Projects} />
+
+      <Route path="/rfc" component={Proposals} />
+
+      <Route path="/rfc/thank-you" component={ProposalsThankYouPage} />
+
+      <Route path="/rfc/example" component={AudioExample} />
+
+      <Route path="/login" component={AudioExample} />
+
+      <Route path="/signup" component={AudioExample} />
+
+      <Route path="/logout" component={Logout} />
+
+      <Route path="/questions" component={QuestionsContainer} />
+
+      <Route path="/membership" component={MembershipPortal} showAds={true} />
+
+      <Route
+        path="/membership/change"
+        component={ChangeMembership}
+        showAds={true}
+      />
+
+      <Route
+        path="/membership/inbox"
+        component={MembershipInbox}
+        showAds={true}
+      />
+
+      <Route
+        path="/membership/analytics"
+        component={MembershipAnalytics}
+        showAds={true}
+      />
+
+      <Route
+        path="/membership/downloads"
+        component={MembershipDownloads}
+        showAds={true}
+      />
+
+      <Route path="/profile/playlist" component={UserPlaylist} showAds={true} />
+
+      <Route path="/admin" component={UserPlaylist} />
+
+      <Route path="/admin/cms/pending" component={AdminCmsPending} />
+      <Route path="/admin/cms/recent" component={AdminCmsRecent} />
+      <Route path="/admin/cms/feature" component={AdminCmsFeature} />
+      <Route path="/admin/cms/add_related" component={AdminCmsAddRelated} />
+      <Route
+        path="/admin/cms/recent_related"
+        component={AdminCmsRecentRelated}
+      />
+      <Route path="/admin/orders/new" component={AdminOrdersNew} />
+      <Route
+        path="/admin/orders/processing"
+        component={AdminOrdersProcessing}
+      />
+      <Route path="/admin/emails/send" component={AdminEmailsSend} />
+      <Route path="/admin/job/add" component={AdminAddJob} />
+      <Route path="/admin/login" component={Login} />
+      <Route path="/admin/tse" component={Explorer} />
+
+      <Route path="/drip-result" component={DripThankYou} showAds={true} />
+
+      <Route
+        path="/drip-unsubscribe"
+        component={DripUnsubscribe}
+        showAds={true}
+      />
+
+      <Route path="/analytics" component={Analytics} />
+
+      <Route path="/wp-login.php" component={DontHackMe} />
+
+      <Route path="/l/snl-impact" component={SnlImpact} />
+
+      <Route path="/privacy" component={PrivacyPageContainer} />
+
+      <Route path="/contributors/:contributor" component={ContributorPage} />
+
+      <Route path="/search" component={SERP} />
+
+      <Route path="/live" component={Live} />
+
       <Route path="/404" component={NotFound} />
+
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
