e38fed858ed21760d8072b61e6ce7a3501c1f8e6
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-05-22 16:59:44 +0300

    Initial Link component commit

---
+++ shared/Layout/Components/Link/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const wrapClick = (fn) => {
+  window.scrollTo(0, 0)
+  fn()
+}
+
+export default ({ children, onClick = () => {}, ...rest }) => (
+  <Link {...rest} onClick={wrapClick(onClick)}>
+    {children}
+  </Link>
+)
