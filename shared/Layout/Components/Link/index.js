28ec15fd8ff71561b7fdb35d737c4074bf3f9ef5
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-05-23 11:58:42 +0300

    Merge conflict fixes

--- shared/Layout/Components/Link/index.js
+++ shared/Layout/Components/Link/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const wrapClick = fn => {
+const wrapClick = (fn, e) => {
   window.scrollTo(0, 0)
-  fn()
+  fn(e)
 }
 
 export default ({ children, onClick = () => {}, ...rest }) => (
-  <Link {...rest} onClick={wrapClick(onClick)}>
+  <Link {...rest} onClick={(e) => wrapClick(onClick, e)}>
     {children}
   </Link>
 )
