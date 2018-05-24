90f813ead933f430c88ba2f9ef8e3cfc85ba5bf1
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-05-24 12:44:57 +0300

    Finished email popup logic

--- shared/Popups/Containers/EmailPopupContainer.js
+++ shared/Popups/Containers/EmailPopupContainer.js
@@ -4,11 +4,15 @@ import { open, close } from '../helpers/popup'
 import { read, write } from '../../Tracking/storage'
 import EmailPopup, { KEY as EMAIL_POPUP_KEY } from './EmailPopup'
 
-const TIME_DELAY = 10 * 1000
+const TIME_DELAY = 5 * 1000
+const OFFSET_POS = 320
 
 class EmailPopupContainer extends Component {
   state = {
-    hasSeen: false
+    hasSeen: false,
+    scrolledToOffset: false,
+    idleTimeFinished: false,
+    triggered: false
   }
 
   componentDidMount() {
@@ -24,27 +28,68 @@ class EmailPopupContainer extends Component {
     this.props.dispatch(close(EMAIL_POPUP_KEY))
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.scrolledToOffset && nextState.idleTimeFinished) {
+      this.trigger()
+      this.setState({
+        scrolledToOffset: false,
+        idleTimeFinished: false
+      })
+    }
+  }
+
   startListener() {
     console.log(`startListener`)
-    this.timer = setTimeout(this.trigger, TIME_DELAY)
+
+    window.addEventListener('scroll', this.listenScroll)
+    this.timer = setTimeout(this.idleTimer, TIME_DELAY)
   }
 
   stopListener() {
     console.log(`stopListener`)
+
     clearTimeout(this.timer)
+    window.removeEventListener('scroll', this.listenScroll)
+  }
+
+  listenScroll = e => {
+    if (!this.state.ticking) {
+      console.log(`scroll`, window.scrollY)
+      if (window.scrollY >= OFFSET_POS) {
+        window.requestAnimationFrame(() =>
+          this.setState({
+            ticking: false,
+            scrolledToOffset: true
+          })
+        )
+      }
+
+      this.setState({
+        ticking: false
+      })
+    }
+  }
+
+  idleTimer = () => {
+    console.info(`idleTimer`)
+    this.setState({
+      idleTimeFinished: true
+    })
   }
 
   trigger = () => {
-    alert(`show`)
+    this.stopListener()
+    this.setState({
+      triggered: true
+    })
+    
+    this.props.dispatch(open(EMAIL_POPUP_KEY))
   }
 
-  markSeen = () => {
-    debugger
-    this.write(EMAIL_POPUP_KEY, true)
-  }
+  markSeen = () => write(EMAIL_POPUP_KEY, true)
 
   render() {
-    return <EmailPopup onClose={this.markSeen}/>
+    return <EmailPopup onClose={this.markSeen} />
   }
 }
 
