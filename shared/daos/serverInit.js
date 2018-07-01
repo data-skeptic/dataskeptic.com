3bc89d7a0f61f1d49dd74f7abc352e11acec6cce
Kyle Polich <kyle@dataskeptic.com>
2018-06-30 21:21:19 -0700

    Deb

--- shared/daos/serverInit.js
+++ shared/daos/serverInit.js
@@ -7,6 +7,8 @@ const s3 = new aws.S3()
 
 const proposalsDocs = new aws.DynamoDB.DocumentClient()
 
+require('dotenv').load();
+
 console.log("process.env.BASE_API")
 console.log(process.env.BASE_API)
 
