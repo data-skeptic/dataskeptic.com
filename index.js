8b6c31d06ccbefa09c4e51c9a9e1bd52adcf55a6
Kyle Polich <kyle@dataskeptic.com>
2018-10-02 16:34:22 -0700

    404

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('*', function(req, res){
+    res.render('pages/missing')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
