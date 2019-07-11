c34b6f57ef42784127fece0cc0bf441f18cab423
Cory Armstrong <c0deslave49@gmail.com>
2019-07-11 13:54:18 -0700

    fixed build issue

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,22 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
+if (!process || !process.env || !process.env['NODE_ENV'] || process.env['NODE_ENV'].indexOf('prod') === -1) require('dotenv').config();
+console.log(process.env.NODE_ENV);
 
 const env = {
-  'process.env.NODE_ENV': JSON.stringify(process.env['NODE_ENV']),
-  'process.env.BASE_API': JSON.stringify(process.env['BASE_API']),
-  'process.env.AWS_ACCESS_KEY_ID': JSON.stringify(
-    process.env['AWS_ACCESS_KEY_ID']
-  ),
-  'process.env.AWS_SECRET_ACCESS_KEY': JSON.stringify(
-    process.env['AWS_SECRET_ACCESS_KEY']
-  ),
-  'process.env.AWS_REGION': JSON.stringify(process.env['AWS_REGION']),
-  'process.env.AWS_CONFIG_S3_BUCKET': JSON.stringify(
-    process.env['AWS_CONFIG_S3_BUCKET']
-  )
+  'process.env.NODE_ENV'              : JSON.stringify(process.env['NODE_ENV']),
+  'process.env.BASE_API'              : JSON.stringify(process.env['BASE_API']),
+  'process.env.AWS_ACCESS_KEY_ID'     : JSON.stringify(process.env['AWS_ACCESS_KEY_ID']),
+  'process.env.AWS_SECRET_ACCESS_KEY' : JSON.stringify(process.env['AWS_SECRET_ACCESS_KEY']),
+  'process.env.AWS_REGION'            : JSON.stringify(process.env['AWS_REGION']),
+  'process.env.AWS_CONFIG_S3_BUCKET'  : JSON.stringify(process.env['AWS_CONFIG_S3_BUCKET'])
 }
 
 process.traceDeprecation = true
@@ -37,64 +31,16 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader']
-        })
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
+      { test: /\.(js|jsx)$/, loaders: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+      { test: /\.less$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'less-loader'] }) },
+      { test: /\.json$/, loader: 'json-loader' }
     ]
   },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.HashedModuleIdsPlugin(),
     new webpack.DefinePlugin(env),
-
-    new webpack.optimize.UglifyJsPlugin({
-      test: /\.js($|\?)/i,
-      exclude: /node_modules/,
-      parallel: true,
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true
-      },
-      output: {
-        comments: false
-      }
-    }),
-
     new ExtractTextPlugin({
       filename: 'main.css',
       disable: false,
